3985299a6fb6cc8ea74494751e9310e08b9a1999
Jeff Berlin <jeff@jbrally.com>
2022-10-18 20:31:29 -0400

    Removing code

--- src/Components/Header/ContactNav.js
+++ src/Components/Header/ContactNav.js
@@ -43,26 +43,6 @@ function ContactNav() {
               Wilmington, NC
             </a>
           </Col>
-          {/* <ul className="contact-info">
-            <li className="email">
-              <a href="mailto:jeff@jbrally.com" alt="email" title="E-mail me">
-            <span className="fa fa-envelope"></span>
-            jeff@jbrally.com
-              </a>
-            </li>
-            <li className="phone">
-              <a href="tel:1-302-463-5129" alt="Phone number" title="Call me">
-            <span className="fa fa-phone"></span>
-            (302)-463-5129
-              </a>
-            </li>
-            <li className="location">
-              <a href="https://www.google.com/maps/place/Wilmington,+NC/@34.2068977,-78.1523955,119498m/data=!3m1!1e3!4m5!3m4!1s0x89a9f5a20debaed5:0x5e66493884093032!8m2!3d34.2103894!4d-77.8868117?hl=en" target="_blank" title="View on a map">
-            <span className="fa fa-map-marker"></span>
-            Wilmington, NC
-              </a>
-            </li>
-          </ul> */}
         </Row>
       </Container>
     </div>
